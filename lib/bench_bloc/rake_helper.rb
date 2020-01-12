d4ac8e727ddede7a7a93ecf5ffaca45a2ca7f260
Jeremy <jeremy@laterolabs.com>
2020-01-12 15:41:59 -0800

    fix bench tasks call

--- lib/bench_bloc/rake_helper.rb
+++ lib/bench_bloc/rake_helper.rb
@@ -1,14 +1,14 @@
 def bench_tasks
   Rake.application.tasks.select do |task|
-    task.name.starts_with?("bench") &&
+    task.name.starts_with?("bench_bloc") &&
     !task.name.ends_with?("_util") &&
-    task.name != "bench:all"
+    task.name != "bench_bloc:all"
   end
 end
 
 def option_parser
   OptionParser.new do |opts|
-    opts.banner = "Usage: rake bench:* [options]"
+    opts.banner = "Usage: rake bench_bloc:* [options]"
     opts.on("-r", "--ruby-prof", "Print a RubyProf report") do |rp|
       @options[:ruby_prof] = rp
     end
