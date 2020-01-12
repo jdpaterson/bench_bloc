007b70cceb500a31a51c6969f39d84a20b1d75ec
Jeremy <jeremy@laterolabs.com>
2020-01-12 15:09:55 -0800

    fix railtie

--- lib/bench_bloc.rb
+++ lib/bench_bloc.rb
@@ -1,5 +1,6 @@
-require "bench_bloc/version"
+require 'bench_bloc/version'
 require 'optparse'
+require 'bench_bloc/railtie' if defined?(Rails)
 
 module BenchBloc
   class Error < StandardError; end
