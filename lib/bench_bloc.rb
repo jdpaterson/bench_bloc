6350b921f32593b64771caa71daee991d53f5e7d
J. Paterson <contact@jpatersondev.com>
2020-01-15 23:30:15 -0800

    move bloc helper logic to classes

--- lib/bench_bloc.rb
+++ lib/bench_bloc.rb
@@ -1,12 +1,6 @@
 require 'bench_bloc/version'
-require 'bench_bloc/helpers/benchmark_helpers'
-require 'bench_bloc/helpers/rake_helpers'
-require 'bench_bloc/helpers/ruby_prof_helpers'
 require 'bench_bloc/railtie' if defined?(Rails)
 
 module BenchBloc
   class Error < StandardError; end
-  include BenchBloc::BenchmarkHelpers
-  include BenchBloc::RakeHelpers
-  include BenchBloc::RubyProfHelpers
 end
--- spec/fixtures/namespace_fixture.rb
+++ spec/fixtures/namespace_fixture.rb
@@ -1,12 +1,12 @@
 {
   spec_namespace: {
     spec_task: {
-      desc: "Test Description",
+      description: "Test Description",
       label: -> (obj) {"Test Sleep 3 Seconds"},
-      prof: -> (obj) {
+      profile: -> (obj) {
         sleep(3)
       },
-      to_prof: -> () { "Test Prof" }
+      to_profile: -> () { "Test Prof" }
     }
   }
 }
