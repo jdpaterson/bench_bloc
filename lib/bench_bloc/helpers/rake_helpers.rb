cdc65aed34fe39682fe5391c6e367b7e1df2d541
Jeremy <jeremy@laterolabs.com>
2020-01-17 16:02:17 -0800

    remove unused code

--- lib/bench_bloc/helpers/rake_helpers.rb
+++ lib/bench_bloc/helpers/rake_helpers.rb
@@ -18,34 +18,6 @@ module BenchBloc::RakeHelpers
     end
   end
 
-  def put_namespace key, namespace
-    namespace key do
-      namespace.keys.each do |ns_key|
-        if is_task? namespace[ns_key]
-          put_task ns_key, namespace[ns_key]
-        else
-          put_namespace ns_key, namespace[ns_key]
-        end
-      end
-    end
-  end
-
-  # TODO: Add a ruby-prof method here if argument is passed
-  def put_task key, new_task
-    desc new_task[:desc]
-    task key => :environment do
-      to_profs = [new_task[:to_prof].call].flatten
-      bm_results = bm_run_results new_task, to_profs
-      bm_log_results bm_results, new_task[:desc]
-      # run ruby-prof
-      # format_ruby_prof(run_ruby_prof(new_task[:prof], tp)) if @options[:ruby_prof] == true
-    end
-  end
-
-  def is_task? obj
-    obj.keys.any?(:to_prof)
-  end
-
   def put_options_parser_task
     desc "Options parser for bench tasks"
     task parse_options_util: :environment do
@@ -78,17 +50,6 @@ module BenchBloc::RakeHelpers
     bench_tasks.each do |task|
     Rake::Task[task.name]
       .enhance(['bench_bloc:parse_options_util', 'bench_bloc:clear_tests_util'])
-    end
-  end
-
-  def put_bloc_namespaces bloc
-    bloc.keys.each do |key|
-      namespace :bench_bloc do
-        put_namespace key, bloc[key]
-        put_options_parser_task
-        put_clear_tests_task
-        put_all_task
-      end
     end
   end
 end
